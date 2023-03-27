4b510d85fd67e69b100fb45f37279874d2ee0a5f
Murat03 <muratipekk033@gmail.com>
2023-03-27 14:33:49 +0300

    AccessModifiers

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -12,4 +12,27 @@ namespace AccessModifiers
         {
         }
     }
+    class Customer
+    {
+        protected int Id {  get; set; }
+        public void Save()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+    }
+    class Student : Customer 
+    {
+        public void Save2()
+        {
+            
+        }
+    }
+    public class Course
+    {
+        public string Name { get; set; }
+    }
 }
